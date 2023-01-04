899c574259a97840a2d1e5e695b3e66dd961f77d
Ilogu Ikechukwu <41580536+NnaaIyke@users.noreply.github.com>
2023-01-05 00:22:19 +0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,14 +13,14 @@ terraform {
 provider "azurerm" {
   features {}
   alias           = "prod-sub"
-  subscription_id = "6b6c23ac-12a7-4ddb-9a8e-043f53d430ff"
+  subscription_id = "6b6c89ac-12a7-4ddb-2t6u-043f53d456ff"
 }
 
 #this si the subscription for the sql server
 provider "azurerm" {
   features {}
   alias           = "sql-sub"
-  subscription_id = "52a56327-d767-4866-87c3-8b81ca5fcc22"
+  subscription_id = "52a56327-d767-442-87c3-8b67ca0fcc22"
 }
 
 provider "azurerm" {
